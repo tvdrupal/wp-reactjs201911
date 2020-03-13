1d0a44e386fe2fd3fb030b9ca9ee90e8909e015b
Jason Zhang <jaszhang1@publicisgroupe.net>
2020-03-13 17:11:19 +0800

    Updates

---
+++ precache-manifest.2cc41377c714ae793194dfc517f00893.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a541798fdac130b072e42c7a7d5cac0",
+    "url": "/wp-reactjs201911/index.html"
+  },
+  {
+    "revision": "abc245bd334d995f3d39",
+    "url": "/wp-reactjs201911/static/css/main.8bd5baf6.chunk.css"
+  },
+  {
+    "revision": "3fe3a097b4e4dcffa87c",
+    "url": "/wp-reactjs201911/static/js/2.b8c725c5.chunk.js"
+  },
+  {
+    "revision": "abc245bd334d995f3d39",
+    "url": "/wp-reactjs201911/static/js/main.82b6817c.chunk.js"
+  },
+  {
+    "revision": "35c8f2b3b503f298fe4b",
+    "url": "/wp-reactjs201911/static/js/runtime-main.8151adf4.js"
+  }
+]);
